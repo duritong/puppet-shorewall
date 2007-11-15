caa39d41522604df38d6dfb1d31ec7ffd969e014
am <am@d66ca3ae-40d7-4aa7-90d4-87d79ca94279>
2007-11-15 20:01:46 +0000

    das shorewall beispielscript war alt ...
    git-svn-id: https://svn/ipuppet/trunk/modules/shorewall@91 d66ca3ae-40d7-4aa7-90d4-87d79ca94279

--- manifests/init.pp
+++ manifests/init.pp
@@ -166,7 +166,7 @@ class shorewall {
 	managed_file { routestopped: }
 	define routestopped($host = '-', $options = '') {
 		entry { "routestopped.d/${name}":
-			line => "${name} ${host} ${options}",
+			line => "${interface} ${host} ${options}",
 		}
 	}
 
