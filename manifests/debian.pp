3224ec6a9701b9e9dbeeda433456a980d7e03c1e
mh <mh@immerda.ch>
2009-12-10 21:28:30 +0100

    do we really need to quote the value?

--- manifests/debian.pp
+++ manifests/debian.pp
@@ -1,10 +1,10 @@
 class shorewall::debian inherits shorewall::base {
     case $shorewall_startup {
-      '': { $shorewall_startup = 1 }
+      '': { $shorewall_startup = "1" }
     }
     file{'/etc/default/shorewall':
         #source => "puppet://$server/modules/shorewall/debian/default",
-	content => template("shorewall/debian_default.erb"),
+        content => template("shorewall/debian_default.erb"),
         require => Package['shorewall'],
         notify => Service['shorewall'],
         owner => root, group => 0, mode => 0644;
