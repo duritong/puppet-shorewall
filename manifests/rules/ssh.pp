079a8e915b73b5a969fa4882eba9fe5c814bf4d9
mh <mh@immerda.ch>
2010-10-21 00:12:20 +0200

    introduce parametrized class for ports so we can pass the ports from the sshd module

--- manifests/rules/ssh.pp
+++ manifests/rules/ssh.pp
@@ -1,10 +1,11 @@
-class shorewall::rules::ssh {
-    shorewall::rule { 'net-me-tcp_ssh':
-        source          => 'net',
-        destination     => '$FW',
-        proto           => 'tcp',
-        destinationport => 'ssh',
-        order           => 240,
-        action          => 'ACCEPT';
-    }
+class shorewall::rules::ssh($ports) {
+  $flatted_ports = join($ports,',')
+  shorewall::rule { 'net-me-tcp_ssh':
+    source          => 'net',
+    destination     => '$FW',
+    proto           => 'tcp',
+    destinationport => $flatted_ports,
+    order           => 240,
+    action          => 'ACCEPT';
+  }
 }
