e3eb56497baf04fa4ce83d2d7ccee3cb2b67109b
Micah Anderson <micah@riseup.net>
2011-02-02 21:54:33 -0500

    fix missing shorewall:: prefix on the extension_script define

--- manifests/extension_script.pp
+++ manifests/extension_script.pp
@@ -1,5 +1,5 @@
 # See http://shorewall.net/shorewall_extension_scripts.htm
-define extension_script($script = '') {
+define shorewall::extension_script($script = '') {
     case $name {
         'init', 'initdone', 'start', 'started', 'stop', 'stopped', 'clear', 'refresh', 'continue', 'maclog': {
             shorewall::managed_file { "${name}": }
