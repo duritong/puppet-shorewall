fdc7a438272e1d59d8f9279af2ec10c9cf44f646
mh <mh@immerda.ch>
2011-04-26 03:08:37 +0200

    allow esp traffic from and to me

--- manifests/rules/ipsec.pp
+++ manifests/rules/ipsec.pp
@@ -1,17 +1,29 @@
 class shorewall::rules::ipsec {
-    shorewall::rule { 'net-me-ipsec-udp':
+    shorewall::rule {
+      'net-me-ipsec-udp':
         source          => 'net',
         destination     => '$FW',
         proto           => 'udp',
         destinationport => '500',
         order           => 240,
         action          => 'ACCEPT';
-    }
-    shorewall::rule { 'me-net-ipsec-udp':
+      'me-net-ipsec-udp':
         source          => '$FW',
         destination     => 'net',
         proto           => 'udp',
         destinationport => '500',
+        order           => 240,
+        action          => 'ACCEPT';
+      'net-me-ipsec':
+        source          => 'net',
+        destination     => '$FW',
+        proto           => 'esp',
+        order           => 240,
+        action          => 'ACCEPT';
+      'me-net-ipsec':
+        source          => '$FW',
+        destination     => 'net',
+        proto           => 'esp',
         order           => 240,
         action          => 'ACCEPT';
     }
