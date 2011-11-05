8f033c40a6ff210ce295db2bde025d739f8c3053
mh <mh@immerda.ch>
2011-11-05 14:15:17 +0100

    configure munin things via hiera, allow interface to be chosen

--- manifests/rules/munin.pp
+++ manifests/rules/munin.pp
@@ -1,8 +1,12 @@
-class shorewall::rules::munin {
-    shorewall::params { 'MUNINPORT': value => $munin_port ? { '' => 4949, default => $munin_port } }
-    shorewall::params { 'MUNINCOLLECTOR': value => $munin_collector ? { '' => '127.0.0.1', default => $munin_collector } }
+class shorewall::rules::munin(
+  $munin_port = hiera('munin_port','4949'),
+  $munin_collector = hiera('munin_collector','127.0.0.1'),
+  $collector_interface_source = hiera('munin_collector_interface_source','net')
+){
+    shorewall::params { 'MUNINPORT': value => $munin_port }
+    shorewall::params { 'MUNINCOLLECTOR': value => $munin_collector }
     shorewall::rule{'net-me-munin-tcp':
-        source          => 'net:$MUNINCOLLECTOR',
+        source          => "${collector_interface_source}:\$MUNINCOLLECTOR",
         destination     => '$FW',
         proto           => 'tcp',
         destinationport => '$MUNINPORT',
