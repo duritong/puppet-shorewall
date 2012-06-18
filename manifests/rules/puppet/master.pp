be37ed29e196008209431f1401c040dd200e3019
mh <mh@immerda.ch>
2012-06-18 15:24:12 -0300

    the rest will be included already by the client

--- manifests/rules/puppet/master.pp
+++ manifests/rules/puppet/master.pp
@@ -1,11 +1,10 @@
 class shorewall::rules::puppet::master {
-    include ::shorewall::rules::puppet
-    shorewall::rule { 'net-me-tcp_puppet-main':
-        source          => 'net',
-        destination     => '$FW',
-        proto           => 'tcp',
-        destinationport => '$PUPPETSERVER_PORT,$PUPPETSERVER_SIGN_PORT',
-        order           => 240,
-        action          => 'ACCEPT';
-    }
+  shorewall::rule { 'net-me-tcp_puppet-main':
+    source          => 'net',
+    destination     => '$FW',
+    proto           => 'tcp',
+    destinationport => '$PUPPETSERVER_PORT,$PUPPETSERVER_SIGN_PORT',
+    order           => 240,
+    action          => 'ACCEPT';
+  }
 }
