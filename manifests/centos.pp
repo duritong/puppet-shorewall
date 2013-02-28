66245069f80e62c26d8827252d6bfac914b974ed
mh <mh@immerda.ch>
2013-02-28 19:45:41 +0100

    use the centos class on centos based systems

--- manifests/centos.pp
+++ manifests/centos.pp
@@ -1,6 +1,6 @@
 # things needed on centos
 class shorewall::centos inherits shorewall::base {
-  if $::lsbmajdistrelease == '6' {
+  if $::lsbmajdistrelease > 5 {
     augeas{'enable_shorewall':
       context => '/files/etc/sysconfig/shorewall',
       changes => 'set startup 1',
