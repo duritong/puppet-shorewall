3e04a43fb31de5e1461289d6810a25df50127c6f
mh <mh@immerda.ch>
2013-02-28 19:28:28 +0100

    with the latest updates on EL6 this is needed

--- manifests/centos.pp
+++ manifests/centos.pp
@@ -1,12 +1,13 @@
+# things needed on centos
 class shorewall::centos inherits shorewall::base {
   if $::lsbmajdistrelease == '6' {
-    # workaround for
-    # http://comments.gmane.org/gmane.comp.security.shorewall/26991
-    file{'/etc/shorewall/params':
-      ensure => link,
-      target => '/etc/shorewall/puppet/params',
-      before => Service['shorewall'],
-      require => File['/etc/shorewall/puppet']
+    augeas{'enable_shorewall':
+      context => '/files/etc/sysconfig/shorewall',
+      changes => 'set startup 1',
+      lens    => 'Shellvars.lns',
+      incl    => '/etc/sysconfig/shorewall',
+      require => Package['shorewall'],
+      notify  => Service['shorewall'],
     }
   }
 }
