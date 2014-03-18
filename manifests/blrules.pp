a03b6c47c27100d21cf9d881848a367a3bab20fa
Lebedev Vadim <abraham1901@gmail.com>
2014-03-18 23:00:32 +0400

    * Add example

--- manifests/blrules.pp
+++ manifests/blrules.pp
@@ -1,9 +1,31 @@
+# Manage blrules. For additional information type "man shorewall-blrules"
+#
+# Sample Usage:
+#
+#  shorewall::interface { 'br0':
+#    zone    => 'net',
+#    rfc1918  => true,
+#    options => 'tcpflags,blacklist,nosmurfs,routeback,bridge';
+#  }
+#
+#  class { 'shorewall::blrules':
+#    options         => 'tcpflags,blacklist,nosmurfs,routeback,bridge',
+#    whitelists    =>  [
+#                          "net:10.0.0.1,192.168.0.1 all",
+#                        ],
+#
+#    drops           => [
+#                          'net  all tcp 22', #ssh
+#                       ],
+#  }
+
+
 class shorewall::blrules (
   $whitelists,
   $drops,
 ) {
   file{'/etc/shorewall/puppet/blrules':
-    content => template("shorewall/blrules.erb"),
+    content => template('shorewall/blrules.erb'),
     require => Package['shorewall'],
     notify  => Service['shorewall'],
     owner   => root,
@@ -11,6 +33,3 @@ class shorewall::blrules (
     mode    => 0644;
   }
 }
-
-
-
