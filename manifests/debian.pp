7ef94b2e8431b669af868547f75742438cac80af
Sylvain Veyrié <sylvain.veyrie@keyconsulting.fr>
2014-09-16 15:28:50 +0200

    Non string mode is now deprecated

--- manifests/debian.pp
+++ manifests/debian.pp
@@ -3,7 +3,7 @@ class shorewall::debian inherits shorewall::base {
     content => template("shorewall/debian_default.erb"),
     require => Package['shorewall'],
     notify => Service['shorewall'],
-    owner => root, group => 0, mode => 0644;
+    owner => root, group => 0, mode => '0644';
   }
   Service['shorewall']{
     status => '/sbin/shorewall status'
