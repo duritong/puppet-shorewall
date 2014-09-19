729929980bb0b4a70981d5ea5bcc94d3b15cfc34
Sylvain Veyrié <sylvain.veyrie@keyconsulting.fr>
2014-09-19 18:12:26 +0200

    "in" is now a reserved keyword see https://docs.puppetlabs.com/puppet/latest/reference/lang_reserved.html

--- manifests/zone.pp
+++ manifests/zone.pp
@@ -1,14 +1,14 @@
 define shorewall::zone(
     $type,
     $options = '-',
-    $in = '-',
+    $in_ = '-',
     $out = '-',
     $parent = '-',
     $order = 100
 ){
     $real_name = $parent ? { '-' => $name, default => "${name}:${parent}" }
     shorewall::entry { "zones-${order}-${name}":
-        line => "${real_name} ${type} ${options} ${in} ${out}"
+        line => "${real_name} ${type} ${options} ${in_} ${out}"
     }
 }
 
