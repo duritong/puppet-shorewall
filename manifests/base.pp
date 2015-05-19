216af53871b3c422652fdccc9cae145914044feb
Micah Anderson <micah@riseup.net>
2015-05-19 13:01:09 -0400

    provide a class ordering hint for augeas setup

--- manifests/base.pp
+++ manifests/base.pp
@@ -27,6 +27,8 @@ class shorewall::base {
     }
   } else {
 
+  Class['augeas'] -> Class['shorewall::base']
+
   augeas { 'shorewall_module_config_path':
     changes => 'set /files/etc/shorewall/shorewall.conf/CONFIG_PATH \'"/etc/shorewall/puppet:/etc/shorewall:/usr/share/shorewall"\'',
     lens    => 'Shellvars.lns',
