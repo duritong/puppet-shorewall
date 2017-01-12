c868850e3161c69d01b7caf05192d625fc1df0c1
Marknl <mark@e-rave.nl>
2017-01-12 13:17:21 +0100

    Use $name for backwards compatibility

--- manifests/host.pp
+++ manifests/host.pp
@@ -1,13 +1,9 @@
 define shorewall::host(
     $zone,
-    $host = undef,
+    $host = $name,
     $options = 'tcpflags,blacklist,norfc1918',
     $order ='100'
 ){
-    
-    unless $host == undef {
-      $host = $name
-    }
 
     shorewall::entry{"hosts-${order}-${name}":
         line => "#${name}\n${zone} ${host} ${options}"
