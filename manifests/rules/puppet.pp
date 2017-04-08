75f0f1dbaa0827f64fb0d21db8f4a89296f177e0
mh <mh@immerda.ch>
2017-04-08 14:07:07 +0200

    migrate to dedicated params

--- manifests/rules/puppet.pp
+++ manifests/rules/puppet.pp
@@ -1,9 +1,10 @@
+# outgoing puppet params
 class shorewall::rules::puppet(
   $puppetserver = "puppet.${::domain}",
   $puppetserver_port = 8140,
   $puppetserver_signport = 8141
 ){
-  shorewall::params{
+  shorewall::params4{
     'PUPPETSERVER':             value => $puppetserver;
     'PUPPETSERVER_PORT':        value => $puppetserver_port;
     'PUPPETSERVER_SIGN_PORT':   value => $puppetserver_signport;
