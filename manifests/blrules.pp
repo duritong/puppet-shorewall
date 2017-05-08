dbc02ed9a962ef85739094f4a3cbd4ad009d0dde
mh <mh@immerda.ch>
2017-05-08 22:12:02 +0200

    don't notify the service

--- manifests/blrules.pp
+++ manifests/blrules.pp
@@ -26,7 +26,7 @@ class shorewall::blrules (
   file{'/etc/shorewall/puppet/blrules':
     content => template('shorewall/blrules.erb'),
     require => Package['shorewall'],
-    notify  => Service['shorewall'],
+    notify  => Exec['shorewall_check'],
     owner   => root,
     group   => 0,
     mode    => '0644';
