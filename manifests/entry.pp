05cc652e6912fcec22ad0a1d943be0192a9df758
Marknl <mark@e-rave.nl>
2017-06-26 11:32:49 +0200

    Removed $ensure

--- manifests/entry.pp
+++ manifests/entry.pp
@@ -1,6 +1,5 @@
 # a core wrapper for all kinds of entries
 define shorewall::entry(
-    $ensure     = present,
     $shorewall  = true,
     $shorewall6 = false,
     $line
@@ -8,7 +7,6 @@ define shorewall::entry(
   $parts = split($name,'-')
   if $shorewall {
     concat::fragment{$name:
-      ensure  => $ensure,
       content => "${line}\n",
       order   => $parts[1],
       target  => "/etc/shorewall/puppet/${parts[0]}",
@@ -16,7 +14,6 @@ define shorewall::entry(
   }
   if $shorewall6 and $shorewall::with_shorewall6 {
     concat::fragment{"shorewall6_${name}":
-      ensure  => $ensure,
       content => "${line}\n",
       order   => $parts[1],
       target  => "/etc/shorewall6/puppet/${parts[0]}",
