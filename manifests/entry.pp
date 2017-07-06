aa9d865f8d9a841cc7d4bef70aa5481816a9649e
mh <mh@immerda.ch>
2017-07-06 08:07:08 +0200

    to make the previous commit work, we should also remove the ensure from fragment

--- manifests/entry.pp
+++ manifests/entry.pp
@@ -9,7 +9,6 @@ define shorewall::entry(
     $parts = split($name,'-')
     if $shorewall {
       concat::fragment{$name:
-        ensure  => $ensure,
         content => "${line}\n",
         order   => $parts[1],
         target  => "/etc/shorewall/puppet/${parts[0]}",
@@ -17,7 +16,6 @@ define shorewall::entry(
     }
     if $shorewall6 and $shorewall::with_shorewall6 {
       concat::fragment{"shorewall6_${name}":
-        ensure  => $ensure,
         content => "${line}\n",
         order   => $parts[1],
         target  => "/etc/shorewall6/puppet/${parts[0]}",
