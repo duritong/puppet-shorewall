4b9c6e1714857a702add52b1176eb65d347ac71d
mh <mh@immerda.ch>
2017-08-03 10:17:00 +0200

    make it possible to set v6 and v6 puppetserver

--- manifests/rules/puppet.pp
+++ manifests/rules/puppet.pp
@@ -1,12 +1,30 @@
 # outgoing puppet params
 class shorewall::rules::puppet(
-  $puppetserver = "puppet.${::domain}",
-  $puppetserver_port = 8140,
+  $puppetserver          = "puppet.${::domain}",
+  $puppetserver_v6       = undef,
+  $puppetserver_port     = 8140,
   $puppetserver_signport = 8141
 ){
-  shorewall::params4{
-    'PUPPETSERVER':             value => $puppetserver;
+  shorewall::params{
     'PUPPETSERVER_PORT':        value => $puppetserver_port;
     'PUPPETSERVER_SIGN_PORT':   value => $puppetserver_signport;
+  }
+  if is_ipv4_address($puppetserver){
+    shorewall::params4{
+      'PUPPETSERVER': value => $puppetserver;
+    }
+    if $puppetserver_v6 {
+      shorewall::params6{
+        'PUPPETSERVER': value => $puppetserver;
+      }
+    }
+  } elsif is_ipv6_address($puppetserver){
+    shorewall::params6{
+      'PUPPETSERVER': value => $puppetserver;
+    }
+  } else {
+    shorewall::params{
+      'PUPPETSERVER': value => $puppetserver;
+    }
   }
 }
