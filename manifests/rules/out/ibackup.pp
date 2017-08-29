398b8b94cbeedf08615c926a5a37974bca002943
mh <mh@immerda.ch>
2017-08-29 12:52:56 +0200

    fix param name

--- manifests/rules/out/ibackup.pp
+++ manifests/rules/out/ibackup.pp
@@ -1,6 +1,6 @@
 class shorewall::rules::out::ibackup(
   $backup_host,
-  $shorewall4 = true,
+  $shorewall  = true,
   $shorewall6 = false,
 ){
   shorewall::rule { 'me-net-tcp_backupssh':
@@ -10,7 +10,7 @@ class shorewall::rules::out::ibackup(
     destinationport => 'ssh',
     order           => 240,
     action          => 'ACCEPT',
-    shorewall4      => $shorewall4,
+    shorewall       => $shorewall,
     shorewall6      => $shorewall6,
   }
 }
