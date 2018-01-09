532d79cba8f01cfc3bcd16ae83741aac08bab80f
intrigeri <intrigeri@boum.org>
2018-01-09 21:36:59 +0000

    Don't transitively pass $ensure parameter to concat::fragment
    Follow up on commit 851c51659961724a1457e3de1bbe9591390b1e82.

--- manifests/rules/gitdaemon/absent.pp
+++
@@ -1,5 +0,0 @@
-class shorewall::rules::gitdaemon::absent inherits shorewall::rules::gitdaemon {
-  Shorewall::Rule['net-me-tcp_gitdaemon']{
-    ensure => absent,
-  }
-}
--- manifests/rules/out/ssh/remove.pp
+++
@@ -1,5 +0,0 @@
-class shorewall::rules::out::ssh::remove inherits shorewall::rules::out::ssh {
-  Shorewall::Rule['me-net-tcp_ssh']{
-    ensure => absent,
-  }
-}
