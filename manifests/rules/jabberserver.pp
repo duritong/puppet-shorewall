abd3ace3479b4aa764e182a5b42597f1bdcef8bf
mh <mh@immerda.ch>
2018-11-27 01:07:50 +0100

    moar outgoing ports and new incoming ports

--- manifests/rules/jabberserver.pp
+++ manifests/rules/jabberserver.pp
@@ -4,21 +4,42 @@ class shorewall::rules::jabberserver(
   $open_stun = true,
   $outgoing_ports = '5260,5269,5270,5271,5272'
 ) {
+  if versioncmp($facts['os']['release']['major'],'6') > 0 {
+    $incom_ports = '443,5222,5269,5443,7777'
+    shorewall::rule{
+      'net-me-redirect-jabber_web_redirect':
+        source          => 'net,$FW',
+        destination     => '5443',
+        proto           => 'tcp',
+        destinationport => '443',
+        order           => 240,
+        action          => 'REDIRECT';
+    }
+  } else {
+    $incom_ports = '5222,5223,5269'
+  }
   shorewall::rule {
     'net-me-tcp_jabber':
-        source          => 'net',
-        destination     => '$FW',
-        proto           => 'tcp',
-        destinationport => '5222,5223,5269',
-        order           => 240,
-        action          => 'ACCEPT';
+      source          => 'net',
+      destination     => '$FW',
+      proto           => 'tcp',
+      destinationport => $incom_ports,
+      order           => 240,
+      action          => 'ACCEPT';
     'me-net-tcp_jabber_s2s':
-        source          => '$FW',
-        destination     => 'net',
-        proto           => 'tcp',
-        destinationport => join(any2array($outgoing_ports),','),
-        order           => 240,
-        action          => 'ACCEPT';
+      source          => '$FW',
+      sourceport      => '5269',
+      destination     => 'net',
+      proto           => 'tcp',
+      order           => 240,
+      action          => 'ACCEPT';
+    'me-net-tcp_jabber_s2s_2':
+      source          => '$FW',
+      destination     => 'net',
+      proto           => 'tcp',
+      destinationport => join(any2array($outgoing_ports),','),
+      order           => 240,
+      action          => 'ACCEPT';
   }
 
   if $open_stun {
@@ -26,7 +47,7 @@ class shorewall::rules::jabberserver(
       'net-me-udp_jabber_stun_server':
         source          => 'net',
         destination     => '$FW',
-        proto           => 'udp',
+        proto           => 'udp,tcp',
         destinationport => '3478',
         order           => 240,
         action          => 'ACCEPT';
