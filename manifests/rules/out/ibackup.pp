34c243e9076b41ef085f62c37fd65f3ee1f103bd
mh <mh@immerda.ch>
2020-12-09 21:06:21 +0100

    rework outgoing backup connection

--- manifests/rules/out/ibackup.pp
+++ manifests/rules/out/ibackup.pp
@@ -1,16 +1,30 @@
+# manage outgoing ssh for backup
 class shorewall::rules::out::ibackup(
-  $backup_host,
-  $shorewall  = true,
-  $shorewall6 = false,
+  Array[Stdlib::IP::Address::Nosubnet] $backup_hosts,
 ){
+  $backup_hosts_ipv4 = $backup_hosts.filter |$e| { $e =~ Stdlib::IP::Address::V4::Nosubnet }
+  $backup_hosts_ipv6 = $backup_hosts.filter |$e| { $e =~ Stdlib::IP::Address::V6::Nosubnet }
+
+  if !empty($backup_hosts_ipv4) {
+    shorewall::param4{
+      'IBACKUP_HOST': value => "[${backup_hosts_ipv4.join('],[')}]",
+    }
+  }
+
+  if !empty($backup_hosts_ipv6) {
+    shorewall::param6{
+      'IBACKUP_HOST': value => "[${backup_hosts_ipv6.join('],[')}]",
+    }
+  }
+
   shorewall::rule { 'me-net-tcp_backupssh':
     source          => '$FW',
-    destination     => "net:${backup_host}",
+    destination     => 'net:IBACKUP_HOST',
     proto           => 'tcp',
     destinationport => 'ssh',
     order           => 240,
     action          => 'ACCEPT',
-    shorewall       => $shorewall,
-    shorewall6      => $shorewall6,
+    shorewall       => !empty($backup_hosts_ipv4),
+    shorewall6      => !empty($backup_hosts_ipv6),
   }
 }
