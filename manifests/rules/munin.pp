94ef0556c79d5531f47a863b94590ff8ad286c5f
mh <mh@immerda.ch>
2021-02-14 17:06:04 +0100

    support multiple sources

--- manifests/rules/munin.pp
+++ manifests/rules/munin.pp
@@ -1,10 +1,10 @@
 # outgoing munin rules
 class shorewall::rules::munin (
-  $munin_port       = '4949',
-  $munin_collector  = ['127.0.0.1'],
-  $munin_collector6 = ['::1'],
-  $collector_source = 'net',
-  $shorewall6       = true,
+  Stdlib::Port $munin_port = '4949',
+  Array[Stdlib::IP::Address::V4::Nosubnet] $munin_collector = ['127.0.0.1'],
+  Array[Stdlib::IP::Address::V6::Nosubnet] $munin_collector6 = ['::1'],
+  Array[Sring[1]] $collector_source = ['net'],
+  Boolean $shorewall6 = true,
 ) {
   shorewall::params {
     'MUNINPORT':
@@ -19,13 +19,15 @@ class shorewall::rules::munin (
       'MUNINCOLLECTOR': value => "[${join(Array($munin_collector6),']/128,[')}]/128";
     }
   }
-  shorewall::rule { 'net-me-munin-tcp':
-    source          => "${collector_source}:\$MUNINCOLLECTOR",
-    destination     => '$FW',
-    proto           => 'tcp',
-    destinationport => '$MUNINPORT',
-    order           => 240,
-    shorewall6      => $shorewall6,
-    action          => 'ACCEPT';
+  $collector_source.each |$src| {
+    shorewall::rule { "${src}-me-munin-tcp":
+      source          => "${src}:\$MUNINCOLLECTOR",
+      destination     => '$FW',
+      proto           => 'tcp',
+      destinationport => '$MUNINPORT',
+      order           => 240,
+      shorewall6      => $shorewall6,
+      action          => 'ACCEPT';
+    }
   }
 }
