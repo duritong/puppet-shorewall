7cfd589ad4004f5afced36e0bd9e7ee23ee4587f
mh <mh@immerda.ch>
2021-02-23 16:21:50 +0100

    improve on ipv6

--- manifests/rules/out/rsyslog.pp
+++ manifests/rules/out/rsyslog.pp
@@ -5,18 +5,25 @@ class shorewall::rules::out::rsyslog (
   Boolean $shorewall6 = true,
 ) {
   $server_ipv4 = $server.filter |$e| { $e =~ Stdlib::IP::Address::V4::Nosubnet }
-  $server_ipv6 = $server.filter |$e| { $e =~ Stdlib::IP::Address::V6::Nosubnet }
   if !empty($server_ipv4) {
     shorewall::params4 {
       'RSYSLOG_SERVER':
         value => $server_ipv4.join(',');
     }
   }
-  if $shorewall6 and !empty($server_ipv6) {
-    shorewall::params6 {
-      'RSYSLOG_SERVER':
-        value => "[${server_ipv6.join('],[')}]";
+  if $shorewall::with_shorewall6 {
+    $server_ipv6 = $server.filter |$e| { $e =~ Stdlib::IP::Address::V6::Nosubnet }
+    if $shorewall6 and !empty($server_ipv6) {
+      shorewall::params6 {
+        'RSYSLOG_SERVER':
+          value => "[${server_ipv6.join('],[')}]";
+      }
+      $_shorewall6 = true
+    } else {
+      $_shorewall6 = false
     }
+  } else {
+    $_shorewall6 = false
   }
   # we want to connect to the puppet server
   shorewall::rule { 'me-net-rsyslog_tcp':
@@ -25,7 +32,7 @@ class shorewall::rules::out::rsyslog (
     proto           => 'tcp',
     destinationport => $port,
     order           => 340,
-    shorewall6      => $shorewall6,
+    shorewall6      => $_shorewall6,
     action          => 'ACCEPT';
   }
 }
